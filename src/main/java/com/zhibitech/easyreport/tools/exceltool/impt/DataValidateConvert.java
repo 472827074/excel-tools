a24e43a7a344f644ec74657b841f52dce3b22c8d
yumeng <472827074@qq.com>
2016-07-04 18:35:15 +0800

    当小弟

--- src/main/java/com/zhibitech/easyreport/tools/exceltool/impt/DataValidateConvert.java
+++ src/main/java/com/zhibitech/easyreport/tools/exceltool/impt/DataValidateConvert.java
@@ -1,6 +1,5 @@
 package com.zhibitech.easyreport.tools.exceltool.impt;
 
-import java.util.List;
 import java.util.Map;
 
 import com.zhibitech.easyreport.tools.exceltool.validate.ValidateResult;
@@ -13,14 +12,6 @@ public interface DataValidateConvert<T> {
 
 	public T convertToEntity(String[] cellValues, ValidateResult result,
 			String[][] beanDatas);
-	
-	public T saveEntity(T entity);
-
-	public void saveEntity(List<T> data);
-
-	public T updateEntity(T entity);
-
-	public void updateEntity(List<T> data);
 
 
 }
